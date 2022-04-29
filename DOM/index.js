8b13388a0a626ac6600faa59887622c0e080cc7b
Allan B <allan.biville@outlook.fr>
2022-04-29 21:18:10 +0200

    EventListener...

--- DOM/index.js
+++ DOM/index.js
@@ -24,7 +24,101 @@ btn2.addEventListener('click', () => {
     reponse.style.visibility="visible";
     question.style.background="green";
     reponse.innerHTML = "Bonne réponse !";
+});
 
+const mousemove = document.querySelector('.mousemove');
+window.addEventListener('mousemove', (e) => {
+    // console.log("Mousemove!!");
+    // console.log(e);
+    mousemove.style.left =e.pageX + "px";
+    mousemove.style.top =e.pageY + "px";
+});
+
+window.addEventListener("mousedown", () => {
+    mousemove.style.transform = "scale(2) translate(-25%, -25%)";
+    mousemove.style.border = "2px solid teal";
+});
+
+window.addEventListener("mouseup", () => {
+    mousemove.style.transform = "scale(1) translate(-50%, -50%)";
+    
+    mousemove.style.border = "2px solid red";
+});
+question.addEventListener('mouseenter', () => {
+    question.style.background= "rgba(0,0,0,0.6)";
+});
+question.addEventListener('mouseout', () => {
+    question.style.background= "rgba(0,0,0,1)";
+});
+reponse.addEventListener("mousemove", () => {
+reponse.style.transform="rotate(20deg)";
+});
+
+
+const keypress = document.querySelector('.keypress');
+const key = document.getElementById('key');
+const ring = (key) => {
+    const audio = new Audio();
+    audio.src= key + ".mp3";
+    audio.play();
+};
+document.addEventListener('keypress',(e) => {
+    key.textContent = e.key;
+    // if (e.key === "j"){
+    //     keypress.style.color= "pink";
+    // }
+    switch (e.key) {
+        case 'j': 
+        keypress.style.color= "pink";
+            break;
+        case 'k': 
+            keypress.style.color= "teal";
+            ring('k');
+            break;
+        case 'l': 
+            keypress.style.color= "yellow";
+            break;
+    
+        default:
+            keypress.style.color= "red";
+            break;
+    }
+});
+
+nav = document.querySelector('nav');
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 120){
+        nav.style.top = "0";
+    } else {
+        nav.style.top = "-50px";
+    }
+});
+
+const inputName = document.querySelector('input[type="text"]');
+const select = document.querySelector("select");
+const form = document.querySelector('form');
+let pseudo = [];
+let language = [];
+inputName.addEventListener('input', (e) => {
+    pseudo.push(e.target.value);
+    // pseudo += e.target.value;
+    console.log(pseudo);
+});
+select.addEventListener('input', (e) => {
+    language.push(e.target.value);
+    console.log(language);
+});
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if(cgv.checked){
+        document.querySelector('form > div').innerHTML = `
+        <h3>Pseudo =  ${pseudo} </h3>
+        <h4>Language préféré = ${language}</h4>`;
+    } else {
+        alert('Veuillez accepter les CGV');
+    }
     
 });
+
 
